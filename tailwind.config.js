9f4b817dd264f3863096c6c4f6c00bcf8e5a3d86
Ivan Molto <ivanmolto@gmail.com>
2023-05-14 20:09:52 +0200

    feat: Configure Tailwind font, colors and plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      bit: "#c1ff3c",
+      mant: "#64b3ae",
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [import("@tailwindcss/aspect-ratio"), import("@tailwindcss/forms")],
 };
