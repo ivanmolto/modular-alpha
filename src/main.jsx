31892ebe874707e88287f2c3e22a01b6d57b94c0
Ivan Molto <ivanmolto@gmail.com>
2023-05-29 23:54:15 +0200

    chore: Add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import Index from "./routes/index";
+import TokenMarket from "./routes/token-market";
+import AccountProfiler from "./routes/account-profiler";
+import TokenProfiler from "./routes/token-profiler";
+import TransactionTracer from "./routes/transaction-tracer";
+import ContractInspector from "./routes/contract-inspector";
+import BlockTracker from "./routes/block-tracker";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import NoMatch from "./routes/no-match";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          { path: "market", element: <TokenMarket /> },
+          { path: "accountprofiler", element: <AccountProfiler /> },
+          { path: "tokenprofiler", element: <TokenProfiler /> },
+          { path: "transactiontracer", element: <TransactionTracer /> },
+          { path: "contractinspector", element: <ContractInspector /> },
+          { path: "blocktracker", element: <BlockTracker /> },
+          { path: "*", element: <NoMatch /> },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
