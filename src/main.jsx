3c981b63b6534fd014080ccf62f94086be3c5886
Ivan Molto <ivanmolto@gmail.com>
2023-05-30 16:20:58 +0200

    chore: Set up Apollo Client and React Query

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./index.css";
 import Root from "./routes/root";
 import Index from "./routes/index";
 import TokenMarket from "./routes/token-market";
 import AccountProfiler from "./routes/account-profiler";
+import Account from "./routes/account";
 import TokenProfiler from "./routes/token-profiler";
 import TransactionTracer from "./routes/transaction-tracer";
 import ContractInspector from "./routes/contract-inspector";
 import BlockTracker from "./routes/block-tracker";
-
 import NoMatch from "./routes/no-match";
 import ErrorPage from "./error-page";
+
+const client = new ApolloClient({
+  uri: "https://explorer.testnet.mantle.xyz/graphiql",
+  cache: new InMemoryCache(),
+});
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -26,6 +35,7 @@ const router = createBrowserRouter([
           { index: true, element: <Index /> },
           { path: "market", element: <TokenMarket /> },
           { path: "accountprofiler", element: <AccountProfiler /> },
+          { path: "accounts/:accountId", element: <Account /> },
           { path: "tokenprofiler", element: <TokenProfiler /> },
           { path: "transactiontracer", element: <TransactionTracer /> },
           { path: "contractinspector", element: <ContractInspector /> },
@@ -39,6 +49,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
