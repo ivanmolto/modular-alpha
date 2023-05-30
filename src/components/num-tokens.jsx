b25512bebdd5532e72ec06b67a55a42d7f0e3f61
Ivan Molto <ivanmolto@gmail.com>
2023-05-30 16:33:01 +0200

    chore: Add number of tokens to the address details

--- src/components/num-tokens.jsx
+++ src/components/num-tokens.jsx
@@ -1,4 +1,42 @@
-const NumTokens = () => {
-  return <div>Num Tokens</div>;
+import { useQuery } from "@tanstack/react-query";
+
+function numberWithCommas(rawNumber) {
+  return rawNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getNumTokens(addressHash) {
+  const options = { method: "GET", headers: { accept: "application/json" } };
+  return fetch(
+    `https://explorer.testnet.mantle.xyz/api?module=account&action=tokenlist&address=${addressHash}`,
+    options
+  ).then((response) => response.json());
+}
+
+function NumTok({ addressHash }) {
+  const numTokenQuery = useQuery({
+    queryKey: ["numtokens", addressHash],
+    queryFn: () => getNumTokens(addressHash),
+  });
+
+  if (numTokenQuery.isLoading)
+    return (
+      <div className="text-sm font-normal">Loading number of tokens...</div>
+    );
+  if (numTokenQuery.isError)
+    return (
+      <div className="text-sm font-normal">{`Error! ${error.message}`}</div>
+    );
+  return (
+    <div className="text-md font-medium text-gray-900">
+      <span className="px-1.5 py-0.5 rounded-md bg-mant">
+        {numTokenQuery.data.result.length} Tokens
+      </span>
+    </div>
+  );
+}
+
+const NumTokens = ({ addressHash }) => {
+  return <NumTok addressHash={addressHash} />;
 };
+
 export default NumTokens;
